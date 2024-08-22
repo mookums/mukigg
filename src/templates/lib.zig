7bad697637c181a89ac5eb5f76d31a35814334e3
Muki Kiboigo <muki@kiboigo.com>
2024-08-22 01:40:40 -0700

    use structs instead of fn params

--- src/templates/lib.zig
+++ src/templates/lib.zig
@@ -4,17 +4,21 @@ const base_template = @embedFile("base.html");
 const header_template = @embedFile("components/header.html");
 const home_template = @embedFile("home.html");
 
-pub fn BaseTemplate(comptime title: []const u8, comptime head: []const u8, comptime body: []const u8) []const u8 {
-    return std.fmt.comptimePrint(base_template, .{
-        .title = title,
-        .head = head,
-        .body = body,
-    });
+const BaseFields = struct {
+    title: []const u8 = "",
+    head: []const u8 = "",
+    body: []const u8 = "",
+};
+pub fn BaseTemplate(comptime fields: BaseFields) []const u8 {
+    return std.fmt.comptimePrint(base_template, fields);
 }
 
 pub fn HomeTemplate() []const u8 {
     return std.fmt.comptimePrint(
-        comptime BaseTemplate("home | muki.gg", "", home_template),
+        comptime BaseTemplate(.{
+            .title = "home | muki.gg",
+            .body = home_template,
+        }),
         .{ .header = header_template },
     );
 }
