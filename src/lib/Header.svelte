6bb768977c350e12c63ce28b02bbb0e78b063850
Muki Kiboigo <muki@kiboigo.com>
2025-07-07 15:05:13 -0700

    feat: mostly ported

---
+++ src/lib/Header.svelte
@@ -0,0 +1,23 @@
+<header class="mt-8">
+    <div class="flex flex-col justify-center items-center">
+        <div class="flex mb-1">
+            <h1 class="text-3xl font-bold">
+                <a href="/">muki.gg</a>
+            </h1>
+        </div>
+        <div class="flex">
+            <!-- <p class="mx-2 font-bold">
+                <a class="cursor-pointer" href="/about">about</a>
+            </p> -->
+            <p class="mx-2 font-bold">
+                <a class="cursor-pointer" href="https://github.com/mookums">github</a>
+            </p>
+            <p class="mx-2 font-bold">
+                <a class="cursor-pointer" href="/muki.asc">gpg</a>
+            </p>
+            <!-- <p class="mx-2 font-bold">
+                <a class="cursor-pointer" href="/static/rss.xml">rss</a>
+            </p> -->
+        </div>
+    </div>
+</header>
